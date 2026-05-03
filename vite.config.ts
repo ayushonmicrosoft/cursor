754b46fc5eb062924e2c9d468c7e1efe546f29e8
Your Name <you@example.com>
2026-05-03 10:21:51 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   .gitignore
    	modified:   package.json
    	new file:   src/vitest-setup.ts
    	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -74,6 +74,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/__tests__/setup.ts'],
+    setupFiles: ['./src/vitest-setup.ts', './src/__tests__/setup.ts'],
   },
 })
